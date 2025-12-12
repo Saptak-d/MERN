f34b027a253f0fbdcca59024eb62163cdace5052
saptak dutta <duttasaptak79@gmail.com>
2025-12-12 23:42:32 +0530

    create the controller of todo

---
+++ backend/src/controller/todos.controller.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { Todo } from "../models/todos.models";
+
+
+export const createTodo = async (req,res)=>{
+   try {
+     const {title, content , createdBy} = req.body;
+    if(!title || !content || !createdBy){
+      throw new Error("ALL fileds are required to create an todo ");
+    }
+    const existtodo = await Todo.findOne({$and:[{title},{createdBy}]});
+    if(existtodo){
+        throw new Error("The Todo is alredy exist ");
+    }
+ 
+    const todo = await Todo.create({
+     title,
+     content,
+     createdBy
+    });
+ 
+    return res.status(201).json({
+      message : "Todo is created successfully",
+      todo
+    })
+   } catch (error) {
+       return res.status(400).json({
+      message: error.message,
+    });
+   }
+}
